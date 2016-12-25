9b21be032c5cac75e199bca4f3e28b88513dae26
John MacFarlane <jgm@berkeley.edu>
2016-12-25 15:19:46 -0700

    Added a tokenizer test.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
+import Test.Tasty
+import Test.Tasty.HUnit
+import CommonMark.Lexer
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "CommonMark tests" $
+    [ testGroup "tokenizer tests" [
+       testCase "lexer handles tab positions" $
+          tokenize "hi\tthere" @=?
+           [[Token (1,0) (TStr "hi")
+            ,Token (1,2) TTab
+            ,Token (1,4) (TStr "there")]]
+      ]
+    ]
