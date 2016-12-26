3fb036772325f8da131667c6690cce1a361ed453
John MacFarlane <jgm@berkeley.edu>
2016-12-25 19:52:43 -0700

    More progress on analyzeLine.

--- src/CommonMark/BlockParser.hs
+++ src/CommonMark/BlockParser.hs
@@ -1,9 +1,79 @@
 module CommonMark.BlockParser ()
 where
 import CommonMark.Lexer (Line, Token(..), tokenize)
+import Data.Tree.Zipper
+import Data.Tree
 
-parseBlocks :: [Line] -> BlockTree
-parseBlocks = undefined
+type BlockTree = TreePos Full Block
 
-data BlockTree = BlockTree
+data Block = Block { blockType   :: BlockType
+                   , delimToks   :: [Token]
+                   , contentToks :: [Token]
+                   }
+  deriving (Eq, Show)
+
+data BlockType = BDocument
+               | BBlockQuote
+               | BList
+               | BItem
+               | BParagraph
+               | BHeading
+               | BCodeBlock
+               | BHtmlBlock
+               | BThematicBreak
+  deriving (Eq, Show)
+
+emptyDoc :: Tree Block
+emptyDoc = Node (Block BDocument [] []) []
+
+parseBlocks :: [Line] -> Tree Block
+parseBlocks = toTree . foldr parseLine (fromTree emptyDoc)
+
+parseLine :: Line -> BlockTree -> BlockTree
+parseLine line treepos =
+  error $ show (rest, lastMatched)
+  where (rest, lastMatched) = analyzeLine (reverse (ancestors treepos)) line
+
+-- | Analyze line and return treepos of last matched
+-- container node plus the remainder of the line,
+-- after matched delimiters.
+analyzeLine :: [BlockTree] -> Line -> (BlockTree, Line)
+analyzeLine (tp:tps) line =
+  case tps of
+       []    -> (tp, line)
+       (n:ns) -> case matchMarker n line of
+                      Nothing   -> (tp, line)
+                      Just rest -> analyzeLine ns rest
+analyzeLine [] line = error "analyzeLine [] - should not happen"
+
+matchMarker :: BlockTree -> Line -> Maybe Line
+matchMarker treepos line =
+  case blockType block of
+       BDocument      -> Just line
+       BBlockQuote    -> Nothing
+       BList          -> Nothing
+       BItem          -> Nothing
+       BParagraph     -> Nothing
+       BHeading       -> Nothing
+       BCodeBlock     -> Nothing
+       BHtmlBlock     -> Nothing
+       BThematicBreak -> Nothing
+  where tree' = tree treepos
+        block = rootLabel tree'
+
+ancestors :: BlockTree -> [BlockTree]
+ancestors treepos =
+  case parent treepos of
+        Nothing   -> [treepos]
+        Just tp   -> tp : ancestors tp
+
+test :: BlockTree
+test = fromTree $
+  Node (Block BDocument [] []) [
+          Node (Block BBlockQuote [] []) [
+            Node (Block BParagraph [] [])
+            []
+         ]
+        ]
+
 
