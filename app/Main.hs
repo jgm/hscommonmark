b1b63ee2e89ae97865b35878eebeea4c4192e540
John MacFarlane <jgm@berkeley.edu>
2017-01-08 17:57:22 +0100

    Added a function to parseCommonMark to resolveInlines.

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,10 @@ module Main where
 
 import CommonMark
 
-import CommonMark.Lexer
 import qualified Data.Text.IO as T
 import CommonMark.Util
-import CommonMark.BlockParser
+import CommonMark.Parser
 
 main :: IO ()
-main = do
-  inp <- T.getContents
-  showTree $ parseBlocks $ tokenize inp
+main = T.getContents >>= showTree . parseCommonMark
 
--- src/CommonMark/Parser.hs
+++ src/CommonMark/Parser.hs
@@ -2,10 +2,24 @@ module CommonMark.Parser ( parseCommonMark )
 where
 import CommonMark.InlineParser
 import CommonMark.BlockParser
+import CommonMark.Types
 import CommonMark.Lexer
 import Data.Text (Text)
-import Data.Tree (Tree)
+import Data.Tree (Tree(..))
+import Data.Monoid
 
 parseCommonMark :: Text -> Tree Block
-parseCommonMark = undefined
+parseCommonMark = resolveInlines mempty . parseBlocks . tokenize
 
+resolveInlines :: RefMap -> Tree Block -> Tree Block
+resolveInlines refmap (Node elt ns) = Node
+  elt{ eltType =
+        (case eltType elt of
+              et@(Paragraph{}) -> et{
+                  paragraphContents = parseInlines refmap (contentToks elt)
+                }
+              et@(Heading{}) -> et{
+                  headingContents = parseInlines refmap (contentToks elt)
+                }
+              et           -> et) }
+   (map (resolveInlines refmap) ns)
