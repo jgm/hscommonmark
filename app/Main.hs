bb131ceb0aecf64f567b0e292d17d41ec6402cf0
John MacFarlane <jgm@berkeley.edu>
2017-01-09 23:57:34 +0100

    CLI program: allow command line arguments.

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,20 @@ import CommonMark
 
 import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy.IO as TL
+import qualified Data.Text as T
 import CommonMark.Util
 import CommonMark.Parser
 import CommonMark.Render.Html5
+import System.Environment
+import Data.List (intersperse)
 
 main :: IO ()
 main = do
-  inp <- T.getContents
+  args <- getArgs
+  inp <- case args of
+              [] -> T.getContents
+              xs -> (mconcat . intersperse (T.singleton '\n'))
+                            <$> mapM T.readFile xs
   let parsed = parseCommonMark inp
   showTree parsed
   TL.putStrLn $ renderHtml5 parsed
